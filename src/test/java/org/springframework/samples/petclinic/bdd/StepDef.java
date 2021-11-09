7dabada6befe15e4cb63e1215e58c66d1fd5c943
burbina <brianna.urbina@contino.io>
2021-11-09 17:24:30 -0600

    code offically copied over! Huzzah

--- src/test/java/org/springframework/samples/petclinic/bdd/StepDef.java
+++ src/test/java/org/springframework/samples/petclinic/bdd/StepDef.java
@@ -1,8 +1,38 @@
 package org.springframework.samples.petclinic.bdd;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
 public class StepDef {
+
+	private Scenario scenario;
+
+	@Before
+	public void before(Scenario scenario) {
+		this.scenario = scenario;
+	}
+
+	@Given("they see the add owner section")
+	public void they_see_the_add_owner_section() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("they submit {string} as a new owner")
+	public void they_submit_as_a_new_owner(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("they see the {string} profile")
+	public void they_see_the_profile(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
 
 }
--- src/test/resources/features/manage-owners.feature
+++ src/test/resources/features/manage-owners.feature
@@ -3,9 +3,10 @@ Feature: Manage Owners
     Feature Description: add owner, edit existing owner, add pets to an owner, add vists
 
     Scenario: new owner is added to system
-    Given Start to type your Given step here
-    When Start to type your When step here
-    Then Start to type your Then step here
+    Given they see the add owner section
+    When they submit "Bri Cat Lover" as a new owner
+    Then they see the "Bri" profile
+
     Scenario: new pet is associated to owner
     Scenario: pet profile is updated
     Scenario: a new visit is added
